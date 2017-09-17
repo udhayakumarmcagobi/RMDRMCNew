5753111b281bbf39ddda32ec57c9f7a406ade344
Udhaya <udhayakumarmcagobi@gmail.com>
2017-09-17 15:59:39 +0530

    CheckBox selection related changes - partially completed

--- RMDRMC.Web.Application/CustomScripts/UserManagement.js
+++ RMDRMC.Web.Application/CustomScripts/UserManagement.js
@@ -1,6 +1,35 @@
 ﻿$(document).ready(function () {
 
+    $(document).on("click", ".main-view-check", function () {
+        MaxCheckBoxFlips(this, 1);
+      
+    });
+
+    $(document).on("click", ".main-modify-check", function () {
+        MaxCheckBoxFlips(this, 2)
+
+    });
+
 });
+
+
+function MaxCheckBoxFlips(checkboxObject, pos) {
+    var checkedValue = checkboxObject.checked;
+    
+
+    var tables = $(checkboxObject).closest("table");
+
+
+     tables.find("tbody tr").each(function () {
+         
+
+             $(this).find("td:eq(" + pos + ") input").each(function () {
+                 this.checked = checkedValue;
+             });
+         
+
+    });
+}
 
 function CreateRole() {
     var createRoleData = $("#FormCreateRole").serialize();
@@ -13,7 +42,7 @@ function CreateRole() {
         url: hostPath + '/UserManagement/ManageRole/Create/',
         cache: false,
         beforeSend: function () {
-            
+
         },
         success: function (jsonResult) {
 

2c330a5a337133506c8363f9e77b2b6ddfa0dd9a
Udhaya <udhayakumarmcagobi@gmail.com>
2017-09-17 16:26:46 +0530

    Assembly Info is added and minor changes

--- RMDRMC.Web.Application/Controllers/HomeController.cs
+++ RMDRMC.Web.Application/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace RMDRMC.Web.Application.Controllers
     {
         // GET: Home
         public ActionResult Index()
-        {            
+        {
             return View();
         }
     }
